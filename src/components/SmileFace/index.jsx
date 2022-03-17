c0b60b293ee5ec46ec038c72bf9faa34803a7ad0
Islombek <sultonovislom9717@gmail.com>
2022-03-18 01:21:48 +0500

    make smileFace with react d3

--- src/components/SmileFace/index.jsx
+++ src/components/SmileFace/index.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { arc } from "d3";
 
-const mouthArc = arc()
-  .innerRadius(90)
-  .outerRadius(100)
-  .startAngle(0)
-  .endAngle(Math.PI * 2);
-
 const width = 960;
 const height = 500;
 const centerX = width / 2;
@@ -15,29 +9,30 @@ const strokeWidth = 20;
 const eyeOffsetX = 90;
 const eyeOffsetY = 100;
 const eyeRadius = 40;
+const mouthWidth = 20;
+const mouthRadius = 140;
+
+const mouthArc = arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth)
+  .startAngle(Math.PI / 2)
+  .endAngle((Math.PI * 3) / 2);
+
 const SmileFace = () => {
   return (
     <div>
       <svg width={width} height={height}>
-        <circle
-          cx={centerX}
-          cy={centerY}
-          r={centerY - strokeWidth / 2}
-          fill="yellow"
-          stroke="black"
-          strokeWidth={strokeWidth}
-        />
-        <circle
-          cx={centerX - eyeOffsetX}
-          cy={centerY - eyeOffsetY}
-          r={eyeRadius}
-        />
-        <circle
-          cx={centerX + eyeOffsetX}
-          cy={centerY - eyeOffsetY}
-          r={eyeRadius}
-        />
-        <path d={mouthArc()} />
+        <g transform={`translate(${centerX},${centerY})`}>
+          <circle
+            r={centerY - strokeWidth / 2}
+            fill="yellow"
+            stroke="black"
+            strokeWidth={strokeWidth}
+          />
+          <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+          <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+          <path d={mouthArc()} />
+        </g>
       </svg>
     </div>
   );
