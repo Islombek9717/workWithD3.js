a4487bfa83cb7e68e83588d2c87509ac536c0c39
Islombek <sultonovislom9717@gmail.com>
2022-03-19 02:03:43 +0500

    make more  face with map

--- src/components/SmileFace/index.jsx
+++ src/components/SmileFace/index.jsx
@@ -1,37 +1,39 @@
 import React from "react";
 import BackgroundCircle from "./BackgroundCircle";
 import Eyes from "./Eyes";
+import FaceContainer from "./FaceContainer";
 import Mouth from "./Mouth";
 
-const width = 960;
-const height = 500;
-const centerX = width / 2;
-const centerY = height / 2;
-const strokeWidth = 20;
-const eyeOffsetX = 90;
-const eyeOffsetY = 100;
-const eyeRadius = 40;
-const mouthWidth = 20;
-const mouthRadius = 140;
-
-const SmileFace = () => {
+const SmileFace = ({
+  width,
+  height,
+  centerX,
+  centerY,
+  strokeWidth,
+  eyeOffsetX,
+  eyeOffsetY,
+  eyeRadius,
+  mouthWidth,
+  mouthRadius,
+}) => {
   return (
-    <div>
-      <svg width={width} height={height}>
-        <g transform={`translate(${centerX},${centerY})`}>
-          <BackgroundCircle
-            strokeWidth={strokeWidth}
-            radius={centerY - strokeWidth / 2}
-          />
-          <Eyes
-            eyeOffsetX={eyeOffsetX}
-            eyeOffsetY={eyeOffsetY}
-            eyeRadius={eyeRadius}
-          />
-          <Mouth mouthRadius={mouthRadius} mouthWidth={mouthWidth} />
-        </g>
-      </svg>
-    </div>
+    <FaceContainer
+      width={width}
+      height={height}
+      centerX={centerX}
+      centerY={centerY}
+    >
+      <BackgroundCircle
+        strokeWidth={strokeWidth}
+        radius={centerY - strokeWidth / 2}
+      />
+      <Eyes
+        eyeOffsetX={eyeOffsetX}
+        eyeOffsetY={eyeOffsetY}
+        eyeRadius={eyeRadius}
+      />
+      <Mouth mouthRadius={mouthRadius} mouthWidth={mouthWidth} />
+    </FaceContainer>
   );
 };
 
